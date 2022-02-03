13517c75777a23e4f0a33ee880426e0f9c9ae549
jaw042 <68666195+jaw042@users.noreply.github.com>
2022-02-03 10:45:25 -0600

    lab5 done

---
+++ lab05/01-font-switcher/index.js
@@ -0,0 +1,18 @@
+let currentSize = 1.4;
+
+const makeBigger = () => {
+   currentSize += 0.2;
+   document.querySelector('h1').style.fontSize = `${currentSize}em`;
+   document.querySelector('.content').style.fontSize = `${currentSize + 0.4}em`;
+};
+
+const makeSmaller = () => {
+   currentSize -= 0.2;
+   document.querySelector('h1').style.fontSize = `${currentSize}em`;
+   document.querySelector('.content').style.fontSize = `${currentSize + 0.4}em`;   
+};
+
+
+document.querySelector("#a1").addEventListener('click', makeBigger);
+document.querySelector("#a2").addEventListener('click', makeSmaller);
+
